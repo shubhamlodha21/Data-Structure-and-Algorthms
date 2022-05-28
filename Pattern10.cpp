8a0cd689a4bbaa7800ca568cb21c207966858e6c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2022-05-28 23:10:01 +0530

    Print Pattern

---
+++ Pattern10.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void Pattern(int n)
+{
+    for(int i='A';i<='A'+n ;i++)
+    {
+        for(int j='A';j<='A'+n ;j++)
+        {
+            cout<<char(j)<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+int main()
+{
+    int iNo=0;
+    cout<<"Enter a Number:";
+    cin>>iNo;
+
+
+    Pattern(iNo);
+    return 0;
+}
